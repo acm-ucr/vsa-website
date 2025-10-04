35b4ab6b2d1f60b9e28163440e0b213bb0ac74b1
Kevin-Pember <kevinkzp@gmail.com>
2025-10-03 22:18:08 -0700

    Implementing Flexbox Lesson

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,12 +1,25 @@
-const Example = (props: { text1: string; text2: string; text3: string }) => {
+const Example = (props: {
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+}) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-red-200 p-10">
-      <div className="w-1/2 bg-green-200 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-green-200 p-5 text-left">props.text2</div>
-        <div className="w-full bg-green-200 p-5 text-right">{props.text2}</div>
+    <div className="bg-vsa-yellow-100 font-vsa-main flex w-2/3 flex-col items-center gap-1 rounded-xl p-10">
+      <div className="bg-vsa-green-100 m-8 w-1/2 p-5 text-center text-xl">
+        {props.text1}
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="flex w-1/2 flex-row gap-5">
+        <div className="bg-vsa-green-200 w-full rounded-full p-5 text-center text-xl">
+          {props.text2}
+        </div>
+        <div className="bg-vsa-green-200 w-full rounded-full p-5 text-center text-xl">
+          {props.text3}
+        </div>
+      </div>
+      <div className="bg-vsa-green-300 m-8 w-1/2 p-5 text-center text-xl text-white">
+        {props.text4}
+      </div>
     </div>
   );
 };
