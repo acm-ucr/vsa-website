dc57770eac2aaa248a3950c9a780f2f4b1b986e4
qhgill <qhgill2@gmail.com>
2025-10-11 14:48:03 -0700

    demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Example from "@/components/Example";
+import VSAlogo from "@/public/VSA_logo.svg";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Example text1="adjust" text2="text2" text3="these" />
+      <Example text1="adjust" text2="text2" text3="these" image={VSAlogo} />
     </div>
   );
 };
