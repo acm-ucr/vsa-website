59c83cc2e48b1623285fd3d9a0de94cfe188ac22
Kevin-Pember <kevinkzp@gmail.com>
2025-10-10 22:02:35 -0700

    Initial Components without any styling

--- src/components/gallery/Card.tsx
+++ src/components/gallery/Card.tsx
@@ -1,5 +1,23 @@
-const Card = () => {
-  return <div></div>;
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+
+interface props {
+  link: string
+  image1: StaticImageData
+  alt1: string
+  image2: StaticImageData
+  alt2: string
+}
+const Card: React.FC<props> = ({link, image1, alt1, image2, alt2}) => {
+  return <div>
+    <div>
+      <Link href={link}/>
+    </div>
+    <div>
+      <Image src={image1} alt={alt1}/>
+      <Image src={image2} alt ={alt2}/>
+    </div>
+  </div>;
 };
 
 export default Card;
