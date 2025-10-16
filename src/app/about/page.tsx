b039207e1e4b35248b91ac885e70aea7b637476f
Rosendo <rosendomarquez30@gmail.com>
2025-10-15 21:12:47 -0700

    add to page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,12 @@
+import MajorEventsHeader from "@/components/about/MajorEventsHeader";
+
 const About = () => {
-  return <div className="">About</div>;
+  return (
+    <>
+      <div className="">About</div>;
+      <MajorEventsHeader/>
+    </>
+  );
 };
 
 export default About;
--- src/components/about/MajorEventsHeader.tsx
+++ src/components/about/MajorEventsHeader.tsx
@@ -1,20 +1,16 @@
 import Image from "next/image";
 import headerImage from "@/public/about_decoration.webp";
 
-interface MajorEventsHeaderProps {
-  title: string;
-  subtitle: string;
-}
 
-const MajorEventsHeader = ({ title, subtitle }: MajorEventsHeaderProps) => {
+const MajorEventsHeader = () => {
   return (
     <div className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-8 px-4 py-8 text-center">
       <h1 className="text-vsa-green-300 font-vsa-main text-4xl font-semibold sm:text-5xl md:text-6xl">
-        {title}
+        Our Major Events
       </h1>
 
       <h2 className="text-vsa-brown font-vsa-alt max-w-[90%] text-xl text-ellipsis whitespace-nowrap sm:max-w-[80%] md:text-3xl">
-        {subtitle}
+        Here are some events to expect every year!
       </h2>
 
       <Image
