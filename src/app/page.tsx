1616980942e4610124dafd93bb326a28c573777f
Shikha Suralikal <shikhasuralikal@Shikhas-MacBook-Pro.local>
2025-10-15 23:27:55 -0700

    Created the Find Us component for the hoomepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client"
+import Example from "@/components/Example";
+import FindUs from "@/components/home/FindUs";
+import VSAlogo from "@/public/VSA_logo.svg";
+
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center p-12"></div>
+    <div>
+      <FindUs title="Find Us!" info="Come meet us at tabling by the Bell Tower every Wednesday" image1={VSAlogo} image2={VSAlogo} />
+    </div>
   );
 };
 
 export default Home;
+
+
+
+// <Example text1="adjust" text2="text2" text3="these" image={VSAlogo} />
+// className="flex h-screen w-screen items-center justify-center"
--- src/components/home/FindUs.tsx
+++ src/components/home/FindUs.tsx
@@ -1,5 +1,25 @@
-const FindUs = () => {
-  return <div></div>;
+"use client"
+import Image, { StaticImageData } from "next/image";
+
+interface FindUsProps {
+  title: string
+  info: string
+  image1: StaticImageData
+  image2: StaticImageData
+}
+
+
+const FindUs = ({title, info, image1, image2}: FindUsProps) => {
+  return (
+    <div className="bg-vsa-yellow-100 flex flex-col items-start justify-center p-20"> 
+      <div className="text-vsa-brown text-5xl text-left font-vsa-main font-bold pl-52 p-4">{title}</div>
+      <div className="text-vsa-brown text-2xl font-vsa-alt text-left pl-52 p-4">{info}</div>
+      <div className="w-full flex justify-center items-center mt-6">
+        <Image src={image1} alt="First image of VSA Logo" className="border-vsa-green-400 aspect-auto border-2 object-contain w-75 h-64"/>
+        <Image src={image2} alt="Second image of VSA Logo" className="border-vsa-green-400 aspect-auto border-2 object-contain w-75 h-64"/>
+      </div>
+    </div>
+  );
 };
 
 export default FindUs;
