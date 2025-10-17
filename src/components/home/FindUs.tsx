7776a97d8fe4d24009697385c7581d7d1a9373ec
Shikha Suralikal <shikhasuralikal@Shikhas-MacBook-Pro.local>
2025-10-17 16:04:12 -0700

    Added spacing between title and text and changed h1 tags

--- src/components/home/FindUs.tsx
+++ src/components/home/FindUs.tsx
@@ -3,10 +3,10 @@
 const FindUs = () => {
   return (
     <div className="bg-vsa-yellow-100 flex flex-col items-center justify-center p-20">
-      <h1 className="text-vsa-brown font-vsa-main w-7/12 text-left text-5xl font-bold">
+      <p className="text-vsa-brown font-vsa-main w-7/12 text-left text-5xl font-bold">
         Find Us!
-      </h1>
-      <p className="text-vsa-brown font-vsa-alt w-7/12 text-left text-2xl">
+      </p>
+      <p className="text-vsa-brown font-vsa-alt w-7/12 pt-4 text-left text-2xl">
         Come meet us at tabling by the Bell Tower every Wednesday
       </p>
       <div className="mt-6 flex w-full items-center justify-center">
