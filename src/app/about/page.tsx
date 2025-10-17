673a7bfac3c2ca74107b299ea4d5723e576734b2
Rosendo <rosendomarquez30@gmail.com>
2025-10-17 12:31:48 -0700

    comment changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <>
       <div className="">About</div>;
-      <MajorEventsHeader/>
+      <MajorEventsHeader />
     </>
   );
 };
--- src/components/about/MajorEventsHeader.tsx
+++ src/components/about/MajorEventsHeader.tsx
@@ -1,23 +1,21 @@
 import Image from "next/image";
 import headerImage from "@/public/about_decoration.webp";
 
-
 const MajorEventsHeader = () => {
   return (
     <div className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-8 px-4 py-8 text-center">
-      <h1 className="text-vsa-green-300 font-vsa-main text-4xl font-semibold sm:text-5xl md:text-6xl">
+      <div className="text-vsa-green-300 font-vsa-main text-4xl font-semibold sm:text-5xl md:text-6xl">
         Our Major Events
-      </h1>
+      </div>
 
-      <h2 className="text-vsa-brown font-vsa-alt max-w-[90%] text-xl text-ellipsis whitespace-nowrap sm:max-w-[80%] md:text-3xl">
+      <div className="text-vsa-brown font-vsa-alt max-w-9/10 text-xl sm:max-w-4/5 md:text-3xl">
         Here are some events to expect every year!
-      </h2>
+      </div>
 
       <Image
         src={headerImage}
         alt="About VSA Decoration"
-        className="mx-auto h-auto w-5/12 object-contain"
-        priority
+        className="flex w-5/12 object-contain"
       />
     </div>
   );
