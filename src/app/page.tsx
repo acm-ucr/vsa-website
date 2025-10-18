832ac2d92c8d0ddca770bbfe923188fbb8a53b43
qhgill <qhgill2@gmail.com>
2025-10-17 18:24:05 -0700

    changes?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Example from "@/components/Example";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center p-12">
+    <div className="flex h-screen w-screen flex-col items-center justify-center p-12">
       {DemoData.map(({ text1, text2, text3 }, index) => (
         <Example key={index} text1={text1} text2={text2} text3={text3} />
       ))}
--- src/data/DemoData.ts
+++ src/data/DemoData.ts
@@ -9,4 +9,9 @@ export const DemoData = [
     text2: "cade is the goat",
     text3: "vsa project members are goated",
   },
+  {
+    text1: "java is the best tasting language",
+    text2: "python doesn't taste good",
+    text3: "nobody thinks c++ tastes good :("
+  }
 ];
