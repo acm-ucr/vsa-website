1ee9033d9bdc34193c218b1b3d2f22b3b5989ed4
qhgill <qhgill2@gmail.com>
2025-10-17 17:58:42 -0700

    demo commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import { DemoData } from "@/data/DemoData";
+import Example from "@/components/Example";
+
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center p-12"></div>
+    <div className="flex h-screen w-screen items-center justify-center p-12">
+      {DemoData.map(({ text1, text2, text3 }, index) => (
+        <Example key={index} text1={text1} text2={text2} text3={text3} />
+      ))}
+    </div>
   );
 };
 
---
+++ src/data/DemoData.ts
@@ -0,0 +1,12 @@
+export const DemoData = [
+  {
+    text1: "wow",
+    text2: "yippee",
+    text3: "so cool",
+  },
+  {
+    text1: "luis is the goat",
+    text2: "cade is the goat",
+    text3: "vsa project members are goated",
+  },
+];
