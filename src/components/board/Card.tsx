2bda2e5f7877d41daf4b0e494b7a64555e101ff9
SBSwag10 <sbswag10@gmail.com>
2025-10-20 12:21:28 -0700

    Fixing Board Card As Requested - Adjusted Border Radius

--- src/components/board/Card.tsx
+++ src/components/board/Card.tsx
@@ -12,7 +12,7 @@ const Card = ({ name, image, position }: CardProps) => {
       <Image
         src={image}
         alt={name}
-        className="border-vsa-pink-300 h-32 w-32 rounded-lg border-1 object-contain"
+        className="border-vsa-pink-300 h-32 w-32 rounded-lg border-2 object-contain"
       />
       <div className="text-vsa font-vsa mt-2 mb-1 text-lg">{name}</div>
       <div className="font-vsa-main text-vsa-pink-300 text-lg font-bold">
