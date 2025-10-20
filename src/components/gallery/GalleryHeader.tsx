bf8c2ceb919efb971bb2ae45f429183cb5e1d443
charlesluo06 <charlesluo06@gmail.com>
2025-10-19 22:28:16 -0700

    created gallery header component

---
+++ src/components/gallery/GalleryHeader.tsx
@@ -0,0 +1,17 @@
+interface props {
+  text: string;
+  title: string;
+}
+
+const Header = ({ text, title }: props) => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center">
+      <h1 className="text-vsa-green-400 font-vsa-main text-5xl font-extrabold">
+        {title}
+      </h1>
+      <p className="font-vsa-alt m-5 text-black">{text}</p>
+    </div>
+  );
+};
+
+export default Header;
