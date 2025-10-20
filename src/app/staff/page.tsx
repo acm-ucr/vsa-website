b9f68a5e6f0b78445934a1c93c525ab8ec13e9fc
SBSwag10 <sbswag10@gmail.com>
2025-10-20 12:18:14 -0700

    Fixing Board Card As Requested

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -1,11 +1,10 @@
 import Card from "@/components/board/Card";
-import VSA_logo from "@/public/VSA_logo.svg";
-
+import VSA from "@/public/VSA_logo.svg";
 
 const Staff = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Card name="John Doe" image={VSA_logo} position="President" />
+      <Card name="John Doe" image={VSA} position="President" />
     </div>
   );
 };
--- src/components/board/Card.tsx
+++ src/components/board/Card.tsx
@@ -1,22 +1,21 @@
 import Image from "next/image";
 
-
 interface CardProps {
   name: string;
-  image: string
+  image: string;
   position: string;
 }
 
-const Card = ({name, image, position }: CardProps) => {
+const Card = ({ name, image, position }: CardProps) => {
   return (
     <div className="m-4 flex w-max flex-col items-center justify-center p-4 text-center">
       <Image
         src={image}
         alt={name}
-        className="h-32 w-32 rounded-lg border-1 border-vsa-pink-300 object-contain"
+        className="border-vsa-pink-300 h-32 w-32 rounded-lg border-1 object-contain"
       />
-      <div className="mt-2 mb-1 text-vsa text-lg font-vsa">{name}</div>
-      <div className="text-lg font-bold font-vsa-main text-vsa-pink-300">
+      <div className="text-vsa font-vsa mt-2 mb-1 text-lg">{name}</div>
+      <div className="font-vsa-main text-vsa-pink-300 text-lg font-bold">
         {position}
       </div>
     </div>
