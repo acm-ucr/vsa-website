95a96597b4bdbe4a2cf8122f05fbc1a45167b992
SBSwag10 <sbswag10@gmail.com>
2025-10-23 12:26:29 -0700

    Modified color to customizable prop

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -4,7 +4,12 @@ import VSA from "@/public/VSA_logo.svg";
 const Staff = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Card name="John Doe" image={VSA} position="President" />
+      <Card
+        name="John Doe"
+        image={VSA}
+        position="President"
+        color="border-vsa-pink-300"
+      />
     </div>
   );
 };
--- src/components/board/Card.tsx
+++ src/components/board/Card.tsx
@@ -4,15 +4,16 @@ interface CardProps {
   name: string;
   image: string;
   position: string;
+  color: string;
 }
 
-const Card = ({ name, image, position }: CardProps) => {
+const Card = ({ name, image, position, color }: CardProps) => {
   return (
     <div className="m-4 flex w-max flex-col items-center justify-center p-4 text-center">
       <Image
         src={image}
         alt={name}
-        className="border-vsa-pink-300 h-32 w-32 rounded-lg border-2 object-contain"
+        className={`${color} h-32 w-32 rounded-lg border-2 object-contain`}
       />
       <div className="text-vsa font-vsa mt-2 mb-1 text-lg">{name}</div>
       <div className="font-vsa-main text-vsa-pink-300 text-lg font-bold">
