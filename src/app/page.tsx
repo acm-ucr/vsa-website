3f23ef1f93ff9546649dfb09e056fb5c118b13e7
qhgill <qhgill2@gmail.com>
2025-10-23 15:08:43 -0700

    format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import FindUs from "@/components/home/FindUs";
 
 const Home = () => {
   return (
-      <div className="flex h-screen w-screen items-center justify-center">
-        <FindUs />
-      </div>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <FindUs />
+    </div>
   );
 };
 
