eb0ed3b03f92d9379d212047ebd55a3807f4532a
Shikha Suralikal <shikhasuralikal@Shikhas-MacBook-Pro.local>
2025-10-23 17:52:49 -0700

    Moved array into a data file and combine div classname with image classname

--- src/app/gallery/generalmeetings/page.tsx
+++ src/app/gallery/generalmeetings/page.tsx
@@ -1,29 +1,6 @@
 "use client";
 import GalleryGrid from "@/components/gallery/GalleryGrid";
-
-import image1 from "@/public/gallery/generalmeetings/genmeetings1.webp";
-import image2 from "@/public/gallery/generalmeetings/genmeetings2.webp";
-import image3 from "@/public/gallery/generalmeetings/genmeetings3.webp";
-import image4 from "@/public/gallery/generalmeetings/genmeetings4.webp";
-import image5 from "@/public/gallery/generalmeetings/genmeetings5.webp";
-import image6 from "@/public/gallery/generalmeetings/genmeetings6.webp";
-import image7 from "@/public/gallery/generalmeetings/genmeetings7.webp";
-import image8 from "@/public/gallery/generalmeetings/genmeetings8.webp";
-import image9 from "@/public/gallery/generalmeetings/genmeetings9.webp";
-import image10 from "@/public/gallery/generalmeetings/genmeetings10.webp";
-
-const images = [
-  image10,
-  image8,
-  image1,
-  image2,
-  image7,
-  image5,
-  image4,
-  image3,
-  image9,
-  image6,
-];
+import { images } from "@/data/Galleries/GeneralMeetingsImages";
 
 const GeneralMeetings = () => {
   return (
--- src/components/gallery/GalleryGrid.tsx
+++ src/components/gallery/GalleryGrid.tsx
@@ -10,13 +10,8 @@ const GridCard = ({ images }: GalleryGridProp) => {
   return (
     <div className="grid grid-cols-2 gap-3 p-48">
       {images.map((image, index) => (
-        <div key={index} className="relative aspect-[3/2]">
-          <Image
-            src={image}
-            alt={"General Meetings Image" + (index + 1)}
-            fill
-            className="object-cover"
-          />
+        <div key={index} className="aspect-[3/2] object-cover">
+          <Image src={image} alt={"General Meetings Image" + (index + 1)} />
         </div>
       ))}
     </div>
