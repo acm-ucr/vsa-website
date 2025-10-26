bd845691c37b6b9bcbfa227bbcced7a54a8b4e60
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2025-10-25 19:31:53 -0700

    tweaking width of board and making naming more consistent

--- src/components/board/StaffBoard.tsx
+++ src/components/board/StaffBoard.tsx
@@ -5,9 +5,9 @@ interface BoardProps {
   children: JSX.Element | JSX.Element[];
 }
 
-const Board = ({ title, color, children }: BoardProps) => {
+const StaffBoard = ({ title, color, children }: BoardProps) => {
   return (
-    <div className={` text-${color} max-w-3/4 text-center text-2xl`}>
+    <div className={` text-${color} w-3/4 text-center text-2xl`}>
       <p className="p-5">{title}</p>
       <div className="grid grid-cols-3 place-items-center gap-5">
         {children}
@@ -15,4 +15,4 @@ const Board = ({ title, color, children }: BoardProps) => {
     </div>
   );
 };
-export default Board;
+export default StaffBoard;
