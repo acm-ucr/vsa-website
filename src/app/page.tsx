63b292d40ea4b336a6be9eba7cf381a7423d22b3
SM Kamal <smkamal1005@gmail.com>
2025-10-27 16:32:53 -0700

    Landing Image Implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import FindUs from "@/components/home/FindUs";
-
+import Landing from "@/components/home/Landing";
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-full w-screen flex-col items-center justify-center">
+      <Landing />
       <FindUs />
     </div>
   );
