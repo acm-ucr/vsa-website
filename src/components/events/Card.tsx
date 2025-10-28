9c1f7552807b5663084a4db56d772a8527d32c90
charlesluo06 <charlesluo06@gmail.com>
2025-10-27 20:18:55 -0700

    adjusted width syntax

--- src/components/events/Card.tsx
+++ src/components/events/Card.tsx
@@ -22,7 +22,7 @@ const Card = ({
         <p className="text-3xl font-bold">{day}</p>
         <p className="font-vsa-alt pt-3">{time}</p>
       </div>
-      <div className="border-vsa-green-400 bg-vsa-yellow-100 w-1/1 rounded-r-md border-2 border-l-0 p-4 text-left">
+      <div className="border-vsa-green-400 bg-vsa-yellow-100 w-full rounded-r-md border-2 border-l-0 p-4 text-left">
         <p className="text-vsa-pink-300 text-2xl font-bold">{title}</p>
         <p className="font-bold">{location}</p>
         <p className="font-vsa-alt text-vsa-brown pt-3">{description}</p>
