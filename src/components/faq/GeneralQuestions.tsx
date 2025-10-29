0283df529a9bd7aa11a27e97cb7333e86fda15c2
SM Kamal <smkamal1005@gmail.com>
2025-10-28 22:53:14 -0700

    Comment fixes

--- src/components/faq/GeneralQuestions.tsx
+++ src/components/faq/GeneralQuestions.tsx
@@ -5,12 +5,12 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
-type GeneralQuestionProps = {
+interface GeneralQuestionProps {
   title: string;
   questions: string[];
   answers: string[];
   color: string;
-};
+}
 
 const GeneralQuestions = ({
   title,
@@ -24,24 +24,16 @@ const GeneralQuestions = ({
         <div className="text-vsa-pink-200 mb-4 flex items-center justify-center p-6 text-6xl font-semibold">
           {title}
         </div>
-        <div className="border-vsa-green-400 border-r-2 border-b-2 border-l-2">
-          <Accordion
-            type="single"
-            collapsible
-            className="w-full"
-            defaultValue={questions.length ? "item-0" : undefined}
-          >
+        <div className="border-vsa-green-400 border-x-2 border-b-2">
+          <Accordion type="single" collapsible className="w-full">
             {questions.map((question, i) => (
               <AccordionItem
                 key={`item-${i}`}
                 value={`item-${i}`}
                 className={` ${i % 2 === 1 ? color : "bg-vsa-yellow-100"}`}
               >
-                <AccordionTrigger className="text-vsa-brown flex-row-reverse items-center justify-end rounded-none border-t-2 border-black text-3xl hover:no-underline [&>svg]:h-12 [&>svg]:w-12">
-                  {" "}
-                  <div className="">
-                    {i + 1}. {question}
-                  </div>
+                <AccordionTrigger className="text-vsa-brown cursor-pointer flex-row-reverse items-center justify-end rounded-none border-t-2 border-black text-3xl hover:no-underline [&>svg]:h-12 [&>svg]:w-12">
+                  {i + 1}. {question}
                 </AccordionTrigger>
                 <AccordionContent className="text-vsa-brown flex gap-4 border-t-2 border-black bg-white p-2 text-3xl">
                   <div className="ml-12 p-4">{answers[i]}</div>
