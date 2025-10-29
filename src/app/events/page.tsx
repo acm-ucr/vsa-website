91c54520728a504dfeec1cb8c22c49d92e2b9bf2
SBSwag10 <sbswag10@gmail.com>
2025-10-29 02:07:41 -0700

    "Upcoming Events Card" Ready For Review

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,17 @@
+import UpcomingEvents from "@/components/events/UpcomingEvents";
+
 const Events = () => {
-  return <div className="">Events</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <UpcomingEvents
+        date="month, day"
+        time="12:34 PM"
+        loc="@location"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        color="vsa-pink-300"
+      />
+    </div>
+  );
 };
 
 export default Events;
