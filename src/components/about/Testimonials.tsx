32c55310ec28d749a23efc284c72dfbb3e814480
SBSwag10 <sbswag10@gmail.com>
2025-10-28 21:42:43 -0700

    Touched Up & Refined Testimonial Card

--- src/components/about/Testimonials.tsx
+++ src/components/about/Testimonials.tsx
@@ -9,26 +9,24 @@ interface CardProps {
 
 const Card = ({ name, image, affiliation, text }: CardProps) => {
   return (
-    <div className="flex justify-center p-4">
-      <div className="border-vsa-green-400 flex w-72 flex-col items-center rounded-lg border-2 border-solid bg-white shadow-[6px_6px_0_#439364]">
-        <div className="mt-4 mr-2 ml-2 flex">
-          <Image
-            src={image}
-            alt="Student Image"
-            className="ml-4 h-28 w-28 rounded-lg object-cover"
-          />
-          <div className="w-32 justify-center p-4 text-left">
-            <h3 className="text-md font-vsa-main text-vsa-brown justify-center text-center break-words">
-              {name}
-            </h3>
-            <p className="font-vsa-alt justify-center p-1 text-center text-xs text-black">
-              {affiliation}
-            </p>
-          </div>
+    <div className="border-vsa-green-400 shadow-[6px_6px_0_theme('colors.vsa-green-300')] flex w-1/5 flex-col items-center justify-center rounded-lg border-2 bg-white p-4">
+      <div className="x-2 mt-4 flex">
+        <Image
+          src={image}
+          alt="Student Image"
+          className="ml-4 h-28 w-28 rounded-lg object-cover"
+        />
+        <div className="w-32 justify-center p-4 text-left">
+          <p className="text-md font-vsa-main text-vsa-brown justify-center text-center">
+            {name}
+          </p>
+          <p className="font-vsa-alt justify-center p-1 text-center text-xs text-black">
+            {affiliation}
+          </p>
         </div>
-        <div className="w-64 p-4 break-words">
-          <p className="font-vsa-alt text-vsa-brown text-sm">{text}</p>
-        </div>
+      </div>
+      <div className="w-64 p-4">
+        <p className="font-vsa-alt text-vsa-brown text-sm">{text}</p>
       </div>
     </div>
   );
