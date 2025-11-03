194696db7694e3a274fe50fa0f977244a5bb97c2
Nimrah S <nimrahs@ucr-secure-43-10-13-220-251.wnet.ucr.edu>
2025-11-03 15:37:23 -0800

    welcome week

--- src/app/gallery/welcomeweek/page.tsx
+++ src/app/gallery/welcomeweek/page.tsx
@@ -1,5 +1,17 @@
-const WelcomeWeek = () => {
-  return <div className="">Welcome Week</div>;
+import GalleryGrid from "@/components/gallery/GalleryGrid";
+import Header from "@/components/gallery/GalleryHeader";
+import { images } from "@/data/Galleries/welcomeWeek";
+
+const GeneralMeetings = () => {
+  return (
+    <div className="p-48 text-2xl">
+      <Header
+        title="Welcome Week"
+        text="Stay in the loop and connect with the VSA fam at our General Meetings, held three times each quarter, usually every two weeks! These meetings are the perfect way to get updates on upcoming events, learn how to get more involved, and hang out with other members through fun activities and games. Each meeting wraps up with a fundraiser, so you can support VSA while enjoying great food or treats. Whether you're a returning member or new to the club, General Meetings are a fun and easy way to stay connected all year long!"
+      />
+      <GalleryGrid images={images} />
+    </div>
+  );
 };
 
-export default WelcomeWeek;
+export default GeneralMeetings;
---
+++ src/data/Galleries/welcomeWeek.ts
@@ -0,0 +1,15 @@
+import welcomeweekImage1 from "@/public/gallery/welcomeweek/welcomeweek1.webp";
+import welcomeweekImage2 from "@/public/gallery/welcomeweek/welcomeweek2.webp";
+import welcomeweekImage3 from "@/public/gallery/welcomeweek/welcomeweek3.webp";
+import welcomeweekImage4 from "@/public/gallery/welcomeweek/welcomeweek4.webp";
+import welcomeweekImage5 from "@/public/gallery/welcomeweek/welcomeweek5.webp";
+import bigLotus from "@/public/big_lotus.svg";
+
+export const images = [
+  bigLotus,
+  welcomeweekImage1,
+  welcomeweekImage2,
+  welcomeweekImage3,
+  welcomeweekImage4,
+  welcomeweekImage5,
+];
