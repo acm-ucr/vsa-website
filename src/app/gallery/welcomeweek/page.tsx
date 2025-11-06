90f8c55b683f6405b71fb51c341656bfacaab50b
Nimrah S <nimrahs@Nimrahs-MacBook-Pro.local>
2025-11-06 13:18:15 -0800

    fixed ordering and spacing

--- src/app/gallery/welcomeweek/page.tsx
+++ src/app/gallery/welcomeweek/page.tsx
@@ -1,6 +1,11 @@
-import GalleryGrid from "@/components/gallery/GalleryGrid";
+import Image from "next/image";
 import Header from "@/components/gallery/GalleryHeader";
-import { images } from "@/data/Galleries/welcomeWeek";
+import welcomeweekImage1 from "@/public/gallery/welcomeweek/welcomeweek1.webp";
+import welcomeweekImage2 from "@/public/gallery/welcomeweek/welcomeweek2.webp";
+import welcomeweekImage3 from "@/public/gallery/welcomeweek/welcomeweek3.webp";
+import welcomeweekImage4 from "@/public/gallery/welcomeweek/welcomeweek4.webp";
+import welcomeweekImage5 from "@/public/gallery/welcomeweek/welcomeweek5.webp";
+import bigLotus from "@/public/big_lotus.svg";
 
 const WelcomeWeek = () => {
   return (
@@ -9,7 +14,26 @@ const WelcomeWeek = () => {
         title="Welcome Week"
         text="Join us for VSA Welcome Week, a full week of fun, fundraising, and community as we kick off the school year! Throughout Week 1, we’ll be hosting a variety of events including field day games, fundraiser opportunities, and a picnic. It's a great way to connect with new friends, win prizes, and support our organization. The week wraps up with our first General Meeting, where you’ll get the inside scoop on what’s coming up for the school year!"
       />
-      <GalleryGrid images={images} />
+      <div className="grid grid-cols-2 gap-3 pt-8">
+        <Image
+          src={bigLotus}
+          alt="Welcome Week"
+          className="aspect-[3/2] w-full"
+        />
+        <Image src={welcomeweekImage1} alt="Welcome Week" />
+      </div>
+      <div className="grid grid-cols-3 gap-3 pt-8">
+        <Image src={welcomeweekImage3} alt="Welcome Week" />
+        <Image
+          src={welcomeweekImage5}
+          alt="Welcome Week"
+          className="col-span-2 h-full"
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-3 pt-8">
+        <Image src={welcomeweekImage2} alt="Welcome Week" />
+        <Image src={welcomeweekImage4} alt="Welcome Week" />
+      </div>
     </div>
   );
 };
