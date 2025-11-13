ed3331e5d91f12776a46457dd36e290936134377
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2025-11-13 15:51:38 -0800

    Creating Fiiler Data

---
+++ src/data/Events/PastEventsInfo.ts
@@ -0,0 +1,20 @@
+export const PastEvents = [
+  {
+    title: "Event 1",
+    location: "Location",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    month: "Past",
+    day: "Event",
+    time: "202X",
+  },
+  {
+    title: "Event 2",
+    location: "Location",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    month: "Past",
+    day: "Event",
+    time: "202X",
+  },
+];
