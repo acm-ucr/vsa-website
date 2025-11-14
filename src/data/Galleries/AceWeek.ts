cdde0aed2e805320f2b683a4a97a6986304e1354
Kaesha Inocencio <freeforallyeet123@gmail.com>
2025-11-13 22:04:30 -0800

    added ace week gallery

---
+++ src/data/Galleries/AceWeek.ts
@@ -0,0 +1,27 @@
+import aceweek1 from "@/public/gallery/aceweek/aceweek1.webp";
+import aceweek2 from "@/public/gallery/aceweek/aceweek2.webp";
+import aceweek3 from "@/public/gallery/aceweek/aceweek3.webp";
+import aceweek4 from "@/public/gallery/aceweek/aceweek4.webp";
+import aceweek5 from "@/public/gallery/aceweek/aceweek5.webp";
+import aceweek6 from "@/public/gallery/aceweek/aceweek6.webp";
+import aceweek7 from "@/public/gallery/aceweek/aceweek7.webp";
+import aceweek8 from "@/public/gallery/aceweek/aceweek8.webp";
+import aceweek9 from "@/public/gallery/aceweek/aceweek9.webp";
+import aceweek10 from "@/public/gallery/aceweek/aceweek10.webp";
+import aceweek11 from "@/public/gallery/aceweek/aceweek11.webp";
+import lotus from "@/public/big_lotus.svg";
+
+export const AceWeekImages = [
+  aceweek2,
+  lotus,
+  aceweek11,
+  aceweek5,
+  aceweek6,
+  aceweek9,
+  aceweek4,
+  aceweek3,
+  aceweek7,
+  aceweek1,
+  aceweek8,
+  aceweek10,
+];
