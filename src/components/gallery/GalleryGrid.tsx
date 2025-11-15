3aed08ba716a88fdc0ccf717d72df1510c8bc7c9
qhgill <qhgill2@gmail.com>
2025-11-14 18:35:44 -0800

    format

--- src/components/gallery/GalleryGrid.tsx
+++ src/components/gallery/GalleryGrid.tsx
@@ -10,7 +10,12 @@ const GridCard = ({ images }: GalleryGridProp) => {
   return (
     <div className="grid grid-cols-2 gap-3 pt-8">
       {images.map((image, index) => (
-          <Image src={image} alt={"General Meetings Image" + (index + 1)} key={index} className="aspect-[3/2] object-cover" />
+        <Image
+          src={image}
+          alt={"General Meetings Image" + (index + 1)}
+          key={index}
+          className="aspect-[3/2] object-cover"
+        />
       ))}
     </div>
   );
