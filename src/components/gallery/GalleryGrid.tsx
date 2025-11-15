0998e06488c0c24d47e2ff2ea347c782a64b3933
SBSwag10 <sbswag10@gmail.com>
2025-11-14 18:41:16 -0800

    Responsified General Meeting Gallery Page
    Resolves #91

--- src/components/gallery/GalleryGrid.tsx
+++ src/components/gallery/GalleryGrid.tsx
@@ -8,7 +8,7 @@ interface GalleryGridProp {
 
 const GridCard = ({ images }: GalleryGridProp) => {
   return (
-    <div className="grid grid-cols-2 gap-3 pt-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 md:gap-3 md:pt-8">
       {images.map((image, index) => (
         <div key={index} className="aspect-[3/2] object-cover">
           <Image src={image} alt={"General Meetings Image" + (index + 1)} />
