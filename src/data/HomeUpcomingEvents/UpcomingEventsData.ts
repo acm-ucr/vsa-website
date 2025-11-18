3717204c28c214fe485b82d03e3716db1ec17f2c
qhgill <qhgill2@gmail.com>
2025-11-17 21:25:52 -0800

    fix bg color

--- src/data/HomeUpcomingEvents/UpcomingEventsData.ts
+++ src/data/HomeUpcomingEvents/UpcomingEventsData.ts
@@ -4,20 +4,20 @@ export const HomeUpcomingEventsInfo = [
     time: "12:34pm",
     loc: "@location",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    color: "vsa-pink-300",
+    color: "bg-vsa-pink-300",
   },
   {
     date: "month, day",
     time: "12:34pm",
     loc: "@location",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    color: "vsa-pink-200",
+    color: "bg-vsa-pink-200",
   },
   {
     date: "month, day",
     time: "12:34pm",
     loc: "@location",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    color: "vsa-pink-100",
+    color: "bg-vsa-pink-100",
   },
 ];
