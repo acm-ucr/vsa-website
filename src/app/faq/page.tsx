bf3a206342afcb9ff8b997e9efab70446af4ac4f
Shikha Suralikal <shikhasuralikal@Shikhas-MacBook-Pro.local>
2025-11-18 22:46:41 -0800

    Implemented membership and other related questions on faq page

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,18 +1,46 @@
 import GeneralQuestions from "@/components/faq/GeneralQuestions";
 import { GeneralQuestions as generalData } from "@/data/Faq/GeneralQuestions";
+import { MembershipQuestions } from "@/data/Faq/MembershipQuestions";
+import { OtherQuestions } from "@/data/Faq/OtherQuestions";
 
 const FAQ = () => {
-  const title = "General Questions";
-  const questions = generalData.map((index) => index.question);
-  const answers = generalData.map((index) => index.answer);
-  const color = "bg-vsa-pink-100";
+  const questions1 = generalData.map((index) => index.question);
+  const answers1 = generalData.map((index) => index.answer);
+
+  const questions2 = generalData.map((index) => index.question);
+  const answers2 = generalData.map((index) => index.answer);
+
+  const questions3 = generalData.map((index) => index.question);
+  const answers3 = generalData.map((index) => index.answer);
+
   return (
-    <GeneralQuestions
-      title={title}
-      questions={questions}
-      answers={answers}
-      color={color}
-    />
+    <div>
+      <GeneralQuestions
+        title="General Questions"
+        questions={questions1}
+        answers={answers1}
+        color="bg-vsa-pink-100"
+        titleColor="text-vsa-pink-200"
+      />
+
+    
+      <GeneralQuestions
+        title="Membership Questions"
+        questions={questions2}
+        answers={answers2}
+        color="bg-vsa-green-100"
+        titleColor="text-vsa-green-300"
+      />
+
+      
+      <GeneralQuestions
+        title="Other Related Questions"
+        questions={questions3}
+        answers={answers3}
+        color="bg-vsa-pink-100"
+        titleColor="text-vsa-pink-200"
+      />
+    </div>
   );
 };
 
---
+++ src/data/Faq/OtherQuestions.ts
@@ -0,0 +1,17 @@
+export const OtherQuestions = [
+    {
+      question: "Do I have to pay to attend all events?",
+      answer:
+        "No, not at all! Many of our events are free, including our General Meetings and more. Just keep an eye on our Instagram for updates.",
+    },
+    {
+      question: "What is UVSA and how is it related to UCR VSA?",
+      answer:
+        "UVSA stands for Union of Vietnamese Student Associations. It’s a larger network that connects VSAs across Southern California.",
+    },
+    {
+      question: "What is an “external”?",
+      answer:
+        "Externals are events hosted by VSAs from other schools. These are great opportunities to meet new people and engage with the larger VSA community across different campuses.",
+    },
+];
