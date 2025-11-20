4de5d79ec28f304e2edb2412bd4b37482e770cea
andrewchen14 <andrewseanchen@gmail.com>
2025-11-20 02:00:22 -0800

    fixed alignment and sizing of cards to fit staggering

--- src/components/about/TestimonialCard.tsx
+++ src/components/about/TestimonialCard.tsx
@@ -9,20 +9,20 @@ interface CardProps {
 
 const Card = ({ name, image, affiliation, text }: CardProps) => {
   return (
-    <div className="border-vsa-green-400 shadow-[6px_6px_0_theme('colors.vsa-green-300')] mt-10 mb-4 ml-16 flex w-2/3 flex-col items-start justify-center rounded-lg border-2 bg-white p-4">
+    <div className="border-vsa-green-400 shadow-[6px_6px_0_theme('colors.vsa-green-300')] mt-10 mb-4 ml-16 flex w-3/4 flex-col items-start justify-center rounded-lg border-2 bg-white p-4">
       <div className="flex items-start gap-2">
         <Image
           src={image}
           alt="Student Image"
-          className="h-28 w-28 rounded-lg object-cover"
+          className="h-28 w-28 ml-8 rounded-lg object-cover"
         />
-        <div className="mt-4 ml-10">
+        <div className="mt-4 ml-16">
           <p className="font-vsa-main text-vsa-brown text-lg">{name}</p>
           <p className="font-vsa-alt text-sm text-black">{affiliation}</p>
         </div>
       </div>
 
-      <div className="w-64 p-4">
+      <div className="w-full p-4">
         <p className="font-vsa-alt text-vsa-brown text-sm">{text}</p>
       </div>
     </div>
--- src/components/about/Testimonials.tsx
+++ src/components/about/Testimonials.tsx
@@ -2,19 +2,36 @@ import { testimonialData } from "@/data/TestimonialData";
 import Card from "./TestimonialCard";
 
 const Testimonials = () => {
+  const leftColumn = testimonialData.filter((_, i) => i % 2 === 0);
+  const rightColumn = testimonialData.filter((_, i) => i % 2 === 1);
+
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-      {testimonialData.map((item, index) => (
-        <Card
+    <div className="pb-10 px-50 grid grid-cols-2 gap-2">
+      <div className="flex flex-col gap-6">
+        {leftColumn.map((item, index) => (
+          <Card
           key={index}
           name={item.name}
           affiliation={item.affiliation}
           image={item.image}
           text={item.text}
-        />
-      ))}
+          />
+        ))}
+      </div>
+      <div className="flex flex-col gap-6 pt-40">
+        {rightColumn.map((item, index) => (
+          <Card
+          key={index}
+          name={item.name}
+          affiliation={item.affiliation}
+          image={item.image}
+          text={item.text}
+          />
+        ))}
+      </div>
     </div>
   );
 };
+
 
 export default Testimonials;
