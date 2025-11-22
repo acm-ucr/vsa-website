056654627475c75ad936746e4005a19ae3303aea
sahix-x <kgarc206@ucr.edu>
2025-11-21 18:10:39 -0800

    Fixed image/click sizing for mobile

--- src/components/gallery/Card.tsx
+++ src/components/gallery/Card.tsx
@@ -19,32 +19,32 @@ const Card = ({
   rightalt,
 }: props) => {
   return (
-    <div className="flex w-full flex-col md:w-2/3 md:flex-row">
-      <div className="bg-vsa-green-300 border-vsa-green-400 flex w-full place-items-center rounded-l-2xl border-3 p-4 text-center text-white md:w-1/5 md:rounded-t-none md:rounded-l-2xl md:p-7">
+    <div className="flex w-full flex-row md:w-2/3">
+      <div className="bg-vsa-green-300 border-vsa-green-400 flex w-1/5 place-items-center rounded-l-2xl border-3 p-2 text-center text-white md:p-7">
         <Link
           href={link}
-          className="font-vsa-main text-xl font-black hover:underline md:text-4xl"
+          className="font-vsa-main text-xs font-black break-words hover:underline md:text-4xl"
         >
           click here to see full gallery
-          <p className="font-vsa-alt text-center text-2xl font-black italic md:text-5xl">
+          <p className="font-vsa-alt text-center text-sm font-black italic md:text-5xl">
             →
           </p>
         </Link>
       </div>
-      <div className="bg-vsa-yellow-100 border-vsa-green-400 w-full rounded-b-2xl border-3 border-t-0 p-4 md:w-4/5 md:rounded-r-2xl md:border-l-0 md:p-9">
+      <div className="bg-vsa-yellow-100 border-vsa-green-400 w-4/5 rounded-r-2xl border-3 border-l-0 p-4 md:p-9">
         <p className="font-vsa-main text-vsa-pink-300 pb-3 text-2xl font-black md:pb-5 md:text-5xl">
           {title}
         </p>
-        <div className="flex flex-col gap-2 md:flex-row md:gap-4">
+        <div className="flex flex-row gap-2 md:gap-4">
           <Image
             src={leftimage}
             alt={leftalt}
-            className="border-vsa-green-400 aspect-square w-full border-3 object-cover md:w-1/2"
+            className="border-vsa-green-400 aspect-square w-1/2 border-3 object-cover"
           />
           <Image
             src={rightimage}
             alt={rightalt}
-            className="border-vsa-green-400 aspect-square w-full border-3 object-cover md:w-1/2"
+            className="border-vsa-green-400 aspect-square w-1/2 border-3 object-cover"
           />
         </div>
       </div>
