94ef83354b67f4bba900bc64c986c4accb928795
CadeJordan <cade.m.jordan@gmail.com>
2025-11-21 18:33:12 -0800

    format

--- src/components/home/animationDemo.tsx
+++ src/components/home/animationDemo.tsx
@@ -6,8 +6,7 @@ import VSAlogo from "@/public/VSA_logo.svg";
 
 const fadeFromRight = {
   initial: { opacity: 0, x: 50 },
-  whileInView: { opacity: 1, x: 0, 
-  transition: { duration: 0.9, delay: 0.3 } },
+  whileInView: { opacity: 1, x: 0, transition: { duration: 0.9, delay: 0.3 } },
 };
 
 const FindUs = () => {
