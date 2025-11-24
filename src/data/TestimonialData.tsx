0f3ff6450dacff400390a3f3db8ea28514804497
CadeJordan <cade.m.jordan@gmail.com>
2025-11-23 22:22:21 -0800

    Small data fix

--- src/data/TestimonialData.tsx
+++ src/data/TestimonialData.tsx
@@ -18,22 +18,4 @@ export const testimonialData = [
     image: tempPhoto,
     text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, est eum. Expedita soluta iure suscipit, molestiae quisquam, minima eligendi voluptate asperiores dolore, quasi tempora aspernatur!",
   },
-  {
-    name: "name4",
-    affiliation: "member",
-    image: tempPhoto,
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, est eum. Expedita soluta iure suscipit, molestiae quisquam, minima eligendi voluptate asperiores dolore, quasi tempora aspernatur!",
-  },
-  {
-    name: "name5",
-    affiliation: "member",
-    image: tempPhoto,
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, est eum. Expedita soluta iure suscipit, molestiae quisquam, minima eligendi voluptate asperiores dolore, quasi tempora aspernatur!",
-  },
-  {
-    name: "name6",
-    affiliation: "member",
-    image: tempPhoto,
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, est eum. Expedita soluta iure suscipit, molestiae quisquam, minima eligendi voluptate asperiores dolore, quasi tempora aspernatur!",
-  },
 ];
