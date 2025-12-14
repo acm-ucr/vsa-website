85595bbb1989d75daf8bcc387c586ce5a3c0529d
charlesluo06 <charlesluo06@gmail.com>
2025-12-14 01:39:38 -0800

    implemented responsify + animated socials

--- src/components/about/Socials.tsx
+++ src/components/about/Socials.tsx
@@ -3,14 +3,29 @@ import { FaDiscord } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 import Link from "next/link";
+import { motion } from "motion/react";
+import { Variants } from "motion/react";
+
+const popAnimation: Variants = {
+  initial: { opacity: 0, scale: 0 },
+  whileInView: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1.2,
+      type: "spring",
+      bounce: 0.7,
+    },
+  },
+};
 
 const Socials = () => {
   return (
-    <div className="bg-vsa-yellow-100 flex flex-col items-center justify-center p-23">
+    <div className="bg-vsa-yellow-100 flex flex-col items-center justify-center p-10 md:p-23">
       <h1 className="text-vsa-pink-300 text-3xl font-bold">
         Check Our Socials Out!
       </h1>
-      <p className="font-vsa-alt w-3/5 pt-5 text-center">
+      <p className="font-vsa-alt w-5/6 pt-5 text-center md:w-3/5">
         Kick off the school year with the Vietnamese Student Association at our
         annual VSA Social during Week 0! Whether you're a returning student or
         brand new to campus, this is the perfect chance to meet new people, make
@@ -20,15 +35,33 @@ const Socials = () => {
         get involved throughout the year.
       </p>
       <div className="flex flex-row">
-        <Link href="https://www.google.com/" target="_blank">
-          <FaInstagram className="fill-vsa-pink-200 m-7 mb-10 size-40" />
-        </Link>
-        <Link href="https://www.google.com/" target="_blank">
-          <FaDiscord className="fill-vsa-pink-200 m-7 mb-10 size-40" />
-        </Link>
-        <Link href="https://www.google.com/" target="_blank">
-          <FaTiktok className="fill-vsa-pink-200 m-7 mb-10 size-40" />
-        </Link>
+        <motion.div
+          variants={popAnimation}
+          initial="initial"
+          whileInView="whileInView"
+        >
+          <Link href="https://www.google.com/" target="_blank">
+            <FaInstagram className="fill-vsa-pink-200 m-7 mb-10 size-20 md:size-35" />
+          </Link>
+        </motion.div>
+        <motion.div
+          variants={popAnimation}
+          initial="initial"
+          whileInView="whileInView"
+        >
+          <Link href="https://www.google.com/" target="_blank">
+            <FaDiscord className="fill-vsa-pink-200 m-7 mb-10 size-20 md:size-35" />
+          </Link>
+        </motion.div>
+        <motion.div
+          variants={popAnimation}
+          initial="initial"
+          whileInView="whileInView"
+        >
+          <Link href="https://www.google.com/" target="_blank">
+            <FaTiktok className="fill-vsa-pink-200 m-7 mb-10 size-20 md:size-35" />
+          </Link>
+        </motion.div>
       </div>
     </div>
   );
