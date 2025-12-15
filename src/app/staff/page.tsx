f437d07fefb7a2963f938352472d33a487abf46f
qhgill <qhgill2@gmail.com>
2025-12-14 22:44:41 -0800

    remaining animation issues

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -1,14 +1,21 @@
+"use client";
 import Card from "@/components/board/Card";
 import StaffBoard from "@/components/board/StaffBoard";
 import { ExecutiveStaff } from "@/data/Staff/ExecutiveStaff";
 import { GeneralStaff } from "@/data/Staff/GeneralStaff";
+import { motion } from "motion/react";
 
 const Staff = () => {
   return (
     <div className="flex h-full w-full flex-col items-center text-center">
-      <p className="text-vsa-green-400 m-8 text-4xl font-black">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+        className="text-vsa-green-400 m-8 text-4xl font-black"
+      >
         Meet the Staff
-      </p>
+      </motion.div>
       <StaffBoard title="Executive Staff" text="text-vsa-pink-300">
         {ExecutiveStaff.map(({ name, image, position }, index) => (
           <Card
--- src/components/board/StaffBoard.tsx
+++ src/components/board/StaffBoard.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { motion } from "motion/react";
+
 import { JSX } from "react";
 interface BoardProps {
   title: string;
@@ -7,12 +10,17 @@ interface BoardProps {
 
 const StaffBoard = ({ title, text, children }: BoardProps) => {
   return (
-    <div className={` ${text} mb-14 w-3/4 max-w-2xl text-center text-2xl`}>
+    <motion.div
+      initial={{ opacity: 0, x: 20 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.4 }}
+      className={` ${text} mb-14 w-3/4 max-w-2xl text-center text-2xl`}
+    >
       <p className="p-5">{title}</p>
       <div className="grid grid-cols-1 place-items-center gap-5 sm:grid-cols-2 md:grid-cols-3">
         {children}
       </div>
-    </div>
+    </motion.div>
   );
 };
 export default StaffBoard;
